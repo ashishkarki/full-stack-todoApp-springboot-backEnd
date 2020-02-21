5cf4178391a5f1aa09f1390f4e045f52b420d523
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-02-21 17:27:35 +0545

    Init commit of Spring boot Todo web service based on spring starter

---
+++ src/test/java/com/akarki/todo/rest/todorestfulwebservices/TodoRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akarki.todo.rest.todorestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
